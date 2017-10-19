76c93c2c6a52745012d1fb328bcb0a16ab053405
womkim <337668563@qq.com>
2017-10-19 23:55:20 +0800

    render react home to[

--- ims-react/src/components/Navigator/index.js
+++ ims-react/src/components/Navigator/index.js
@@ -9,23 +9,26 @@ import { Link } from 'react-router-dom'
 import { Menu, Icon } from 'antd'
 const { SubMenu, Item } = Menu
 
+const classname = (arr) => arr.join(' ')
+
 class Navgator extends React.Component {
   render () {
     const { isNavbar, siderFold, darktheme, username, toggleSider } = this.props
-    return <div className="m-nav">
-      {
-        isNavbar
-          ? <div><Icon type="bars" /></div>
-          : <div><Icon type={siderFold ? 'menu-fold' : 'menu-unfold'} /></div>
-      }
-      <Menu
-        onClick={() => {}}
-        mode="horizontal"
-      >
-        <SubMenu title={<span><Icon type="setting" />{username}</span>}>
-          <Item key="logout">Sign out</Item>
-        </SubMenu>
-      </Menu>
+    return <div className={classname(['m-nav', !isNavbar && siderFold ? 'fold-sider' : null])}>
+
+      {isNavbar
+        ? <div className="m-nav-btn"><Icon type="bars" /></div>
+        : <div className="m-nav-btn" onClick={toggleSider}><Icon type={siderFold ? 'menu-fold' : 'menu-unfold'} /></div>}
+      <div className="right-wrapper">
+        <div className="m-nav-btn">
+          <Icon type="home" />
+        </div>
+        <Menu onClick={() => {}} mode="horizontal" >
+          <SubMenu title={<span><Icon type="user" />{username}</span>}>
+            <Item key="logout">Sign out</Item>
+          </SubMenu>
+        </Menu>
+      </div>
     </div>
   }
 }
